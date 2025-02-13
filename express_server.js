5cce12af2610eefa9e648a0a7aa6199df2f73a25
Abdullah Zia <amz1cflp99@gmail.com>
2025-02-12 20:35:53 -0500

    added users object to express_server.js

--- express_server.js
+++ express_server.js
@@ -9,6 +9,21 @@ app.use(cookieParser());
 // Middleware to parse request body
 app.use(express.urlencoded({ extended: true }));
 
+// Users object
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
+
+
 // URL database
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
